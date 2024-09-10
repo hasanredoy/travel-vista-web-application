28a5bc8dc7df4fbd20181af412ea15a8f3b309ad
unknown <redoyhasan4585@gmail.com>
2024-09-10 19:15:32 +0400

    working on banner added title and description

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -2,19 +2,18 @@ import './home.css'
 const Hero = () => {
   return (
 <div
-  className="hero min-h-screen"
+  className="hero min-h-[calc(100dvh-200px)] relative "
   style={{
-    backgroundImage: "url(https://img.freepik.com/free-photo/aerial-view-sandy-beach-with-tourists-swimming-beautiful-clear-sea-water-sumilon-island-beach-landing-near-oslob-cebu-philippines-boost-up-color-processing_1253-981.jpg?t=st=1725964984~exp=1725968584~hmac=16fcc8a2a5f04bfd6244b12f10d2128c9ee02273f4e6f64942d07923fe5ab9ed&w=826)",
+    backgroundImage: "url(https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
   }}>
-  <div className="hero-overlay bg-sky-500 bg-opacity-40"></div>
-  <div className="hero-content text-neutral-content text-center">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
+  <div className="hero-overlay w-full bg-opacity-40 bg-sky-700"></div>
+  <div className=" absolute  top-[20%] left-[10%] ">
+    <div className="max-w-md  text-white">
+      <h1 className="mb-5 text-2xl md:text-3xl lg:text-5xl font-black"><span className=' text-[#BF2EF0]'>Discover</span> the World with Travel Vista</h1>
+      <p className="mb-5 bg-black bg-opacity-10 ">
+      Explore breathtaking destinations, plan unforgettable journeys, and unlock hidden gems across the globe. Your adventure starts here.
       </p>
-      <button className="btn btn-primary">Get Started</button>
+      <button className=" btn-primary">Get Started</button>
     </div>
   </div>
 </div>
