40d17ef48bcaac5a4339f63717a74b03c5c9bc1b
unknown <redoyhasan4585@gmail.com>
2024-09-12 21:27:44 +0400

    added custom heading

--- src/components/reuseble/Heading.jsx
+++ src/components/reuseble/Heading.jsx
@@ -1,9 +1,11 @@
+import { PiAirplaneTiltFill } from "react-icons/pi";
 
 const Heading = ({desc,t1,t2,imp}) => {
   
   return (
     <div>
-      
+      <p className=" text-base font-semibold text-center my-3 flex gap-3 items-center md:text-lg"><PiAirplaneTiltFill></PiAirplaneTiltFill><span>{desc}</span><PiAirplaneTiltFill className=" rotate-180"></PiAirplaneTiltFill></p>
+      <h1 className=" text-2xl font-black text-center my-2 md:text-3xl lg:text-4xl ">{t1}<span className=" bg-gradient-to-r bg-clip-text text-transparent from-[#71fdfd] via-[#4cfdee] to-[#61daf0]">{imp}</span>{t2}</h1>
     </div>
   );
 };
