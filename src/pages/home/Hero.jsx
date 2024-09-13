fb34beff4a57d40d596540fccd624ac550af9ac3
unknown <redoyhasan4585@gmail.com>
2024-09-13 21:12:31 +0400

    working on  hero

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -5,17 +5,17 @@ import './home.css'
 const Hero = () => {
   return (
 <div
-  className="hero min-h-[calc(100dvh-200px)] relative "
+  className="hero min-h-[calc(100dvh-0px)] md:min-h-[calc(100dvh-200px)] relative "
   style={{
     backgroundImage: "url(https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)",
   }}>
   <div className="hero-overlay w-full bg-opacity-50 bg-black"></div>
 
-  <section className=' flex justify-between items-center gap-20 flex-col lg:flex-row  '>
+  <section className=' flex justify-between items-center gap-10 md:gap-14 md:px-10 lg:gap-20 flex-col md:flex-row  '>
   <div className=" ">
-    <div className="max-w-md lg:text-start text-center lg:p-0 pt-10 px-5 pb-0  text-white">
-      <h1 className="mb-5 text-2xl lg:bg-black bg-opacity-15 md:text-3xl lg:text-5xl font-black"><span className=' text-[#FFDC7F]  bg-gradient-to-r bg-clip-text text-transparent from-[#71fdfd] via-[#4cfdee] to-[#61daf0]'>Discover</span> the World with Travel Vista</h1>
-      <p className="mb-5 lg:bg-black text-sm md:text-base lg:text-lg bg-opacity-10 ">
+    <div className="max-w-md md:text-start text-center lg:p-0 pt-10 px-5 pb-0  text-white">
+      <h1 className="mb-5 text-2xl  md:text-3xl lg:text-5xl font-black"><span className=' text-[#FFDC7F]  bg-gradient-to-r bg-clip-text text-transparent from-[#71fdfd] via-[#4cfdee] to-[#61daf0]'>Discover</span> the World with Travel Vista</h1>
+      <p className="mb-5  text-sm md:text-base lg:text-lg bg-opacity-10 ">
       Explore breathtaking destinations, plan unforgettable journeys, and unlock hidden gems across the globe. Your adventure starts here.
       </p>
       <button className=" btn-primary">Book Now</button>
