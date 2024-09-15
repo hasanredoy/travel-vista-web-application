2875e90d2330e86e2d74d8065aa4121fecacc4cb
unknown <redoyhasan4585@gmail.com>
2024-09-15 20:54:50 +0400

    added static footetr

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer/Footer";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
         
        <div> <Navbar></Navbar></div>
         {children}
+       <div> <Footer></Footer></div>
       </body>
     </html>
   );
--- src/pages/home/Discount.jsx
+++ src/pages/home/Discount.jsx
@@ -12,15 +12,8 @@ const Discount = () => {
   return (
  <section className=" flex gap-20  w-[94%] md:w-[90%] lg:w-[85%] mx-auto">
   {/* 1st discount  */}
-    <div className=' relative flex-1 flex items-center justify-center flex-col h-[200px] bg-gradient-to-tr rounded-lg shadow-lg shadow-gray-400 from-[#c8fcf9] via-[#fdc1f3]  to-[#9bfdf8]'>
-     {/* <div className=' absolute top-2 left-0 right-auto'>
-      <Image src={discount40percent} alt='discount 40%' height={60} width={60}></Image>
+    <div className=' relative flex-1 flex items-center justify-center flex-col h-[200px] bg-gradient-to-tr rounded-lg shadow-lg shadow-gray-400 from-[#c8fcf9] via-[#f8ccf1]  to-[#9bfdf8]'>
 
-     </div>
-     <div className=' absolute top-2  right-0 rotate-90'>
-      <Image src={line} alt='line' height={80} width={60}></Image>
-
-     </div> */}
       <h1  className=' text-xl font-black   '>Get Upto 40% discount on your first tour</h1>
      <div className=' flex gap-6'>
       <Image src={discountImage4} alt='discount ' height={100} width={100}></Image>
@@ -28,11 +21,14 @@ const Discount = () => {
      </div>
     </div>
   {/* 2nd discount  */}
-  <div className=' flex-1 h-[400px] bg-gradient-to-r from-[#c8fcf9] via-[#e4b2f8]  to-[#09c4ba]'>
-      <h1>Get Upto 40% Discount </h1>
-      <Image src={discountImage1} alt='discount ' height={100} width={100}></Image>
-      
-    </div>
+  <div className=' relative flex-1 flex items-center justify-center flex-col h-[200px] bg-gradient-to-tl rounded-lg shadow-lg shadow-gray-400 from-[#c8fcf9] via-[#f8ccf1]  to-[#9bfdf8]'>
+
+<h1  className=' text-xl font-black   '>Get Upto 40% discount on your first tour</h1>
+<div className=' flex gap-6'>
+<Image src={discountImage4} alt='discount ' height={100} width={100}></Image>
+
+</div>
+</div>
  </section>
   );
 };
