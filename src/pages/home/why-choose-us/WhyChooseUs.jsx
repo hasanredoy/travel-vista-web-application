8c18bf1cec59c69a256085db168de5492da24eb7
unknown <redoyhasan4585@gmail.com>
2024-09-17 14:30:59 +0400

    working on  why choose us section

--- src/pages/home/why-choose-us/WhyChooseUs.jsx
+++ src/pages/home/why-choose-us/WhyChooseUs.jsx
@@ -3,7 +3,6 @@ import useLoadWhyChooseUsData from "@/hooks/why-choose-us/useLoadWhyChooseUsData
 
 const WhyChooseUs = () => {
   const data = useLoadWhyChooseUsData()
-  console.log(data);
   return (
     <div>
       
