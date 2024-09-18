1822e21a609b7281664141f7778d4a938b6e67cf
unknown <redoyhasan4585@gmail.com>
2024-09-18 13:14:21 +0400

    working on  why choose us section and popular tour section

--- src/pages/home/why-choose-us/WhyChooseUs.jsx
+++ src/pages/home/why-choose-us/WhyChooseUs.jsx
@@ -1,10 +1,12 @@
 'use client'
+import Heading from "@/components/reuseble/Heading";
 import useLoadWhyChooseUsData from "@/hooks/why-choose-us/useLoadWhyChooseUsData";
 
 const WhyChooseUs = () => {
   const data = useLoadWhyChooseUsData()
   return (
     <div>
+      <Heading t1={'Why Choose'} imp={'Travel Vista'}></Heading>
       
     </div>
   );
