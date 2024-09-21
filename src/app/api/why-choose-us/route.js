699212b223fa89e6a9d0015c663edd6d25b92b4c
unknown <redoyhasan4585@gmail.com>
2024-09-21 14:30:27 +0400

    added stats in why choose us section

--- src/app/api/why-choose-us/route.js
+++ src/app/api/why-choose-us/route.js
@@ -14,16 +14,19 @@ const data =[
   {
     "title": "Curated Travel Experiences",
     "description": "At Travel Vista, we offer personalized travel packages that cater to every kind of traveler, ensuring unforgettable experiences tailored to your preferences.",
-    "icon": "https://i.postimg.cc/Dy7vRNJz/experience.png"
+    "icon": "https://i.postimg.cc/Dy7vRNJz/experience.png",
+    "bg":'bg-gradient-to-r rounded-lg shadow-lg shadow-gray-400 from-[#68e3e7] via-[#f9fcfc]  to-[#68e3e7]'
   },
   {
     "title": "Expert Guidance & Support",
     "description": "Our team of travel experts is available 24/7 to assist you with recommendations, bookings, and support, ensuring a smooth and stress-free journey.",
-    "icon": "https://i.postimg.cc/d3xsKsTc/expert-advice.png"
+    "icon": "https://i.postimg.cc/T2m96mgr/customer-service.png",
+     "bg":'bg-gradient-to-r rounded-lg shadow-lg shadow-gray-400 from-[#f8fafa] via-[#68e3e7]  to-[#f9fcfc]'
   },
   {
     "title": "Best Price Guarantee",
     "description": "We work directly with local partners and providers to offer you the best prices without compromising on quality. Enjoy premium travel experiences at unbeatable rates.",
-    "icon": "https://i.postimg.cc/d3xsKsTc/expert-advice.png"
+    "icon": "https://i.postimg.cc/ZYFdHL6D/best-price-2.png",
+       "bg":'bg-gradient-to-r rounded-lg shadow-lg shadow-gray-400 from-[#68e3e7] via-[#f9fcfc]  to-[#68e3e7]'
   }
 ]
--- src/pages/home/why-choose-us/WhyChooseUs.jsx
+++ src/pages/home/why-choose-us/WhyChooseUs.jsx
@@ -34,14 +34,15 @@ const WhyChooseUs = () => {
     <section className="w-[94%] md:w-[90%] lg:w-[85%] mx-auto">
       <Heading desc={'Find Out'}  t1={'Why Choose '} imp={' Travel Vista'}></Heading>
        <div className="">
-       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 p-4">
+       <div className="grid my-10 grid-cols-1 gap-10 sm:grid-cols-2 lg:grid-cols-3 p-4">
       {data.map((stat, index) => (
         <div
           key={index}
-          className={`flex relative items-center p-10 ${stat.bgColor}  rounded-lg shadow-lg`}
+          className={`flex z-40 relative items-center p-10 ${stat.bg}   rounded-lg shadow-lg`}
         >
-          <div className=" w-28 h-28 border border-sky-500 rotate-90 absolute -top-5 -left-5"></div>
-          <img className=" w-10 h-10 " src={stat?.icon} alt="" />
+          <div className=" w-28 z-0 h-28 border-t border-l border-sky-500 - absolute -top-5 -left-5"></div>
+          <div className=" w-28 z-0 h-28 border-t border-l border-sky-500 -rotate-180 absolute -bottom-5 -right-8"></div>
+          <img className=" mr-3 w-10 h-10  text-black " src={stat?.icon} alt="" />
           <div>
             <h3 className="text-lg font-semibold">{stat?.title}</h3>
             <p className="text-2xl font-bold">{stat?.value}</p>
