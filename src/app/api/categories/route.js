4100a5bb4e92087a48c3ec5bcdaae4e32818e1ea
unknown <redoyhasan4585@gmail.com>
2024-09-23 17:57:44 +0400

    make some changes in categories route

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -12,29 +12,29 @@ export const GET = async()=>{
 
 const data = [
   {
-    "category_name": "Adventure",
+    "category": "Adventure",
     "description": "Explore thrilling destinations with activities like hiking, rafting, and mountain climbing for the ultimate adrenaline rush.",
-    "image": "adventure.jpg"
+    "image": "https://i.postimg.cc/dtL2grXr/map.png"
   },
   {
-    "category_name": "Beach Holidays",
+    "category": "Beach Holidays",
     "description": "Relax on stunning beaches with crystal-clear waters, perfect sunsets, and luxurious resorts for a tropical getaway.",
-    "image": "beach.jpg"
+    "image": "https://i.postimg.cc/pLLgwGvk/summer-holidays.png"
   },
   {
-    "category_name": "Cultural Tours",
-    "description": "Immerse yourself in diverse cultures, history, and traditions by visiting ancient landmarks and vibrant local communities.",
-    "image": "cultural.jpg"
+    "category": "Mountain Escapes",
+    "description": "Discover breathtaking mountain ranges, enjoy serene landscapes, and embark on challenging treks amidst nature's beauty.",
+    "image": "https://i.postimg.cc/63XzXqRw/mountain.png"
   },
   {
-    "category_name": "Wildlife Safaris",
+    "category": "Wildlife Safaris",
     "description": "Embark on thrilling safaris to witness wildlife in their natural habitats, from African savannas to Amazon rainforests.",
-    "image": "wildlife.jpg"
+    "image": "https://i.postimg.cc/J0g1sfRr/tree.png"
   },
   {
-    "category_name": "Cruises",
+    "category": "Cruises",
     "description": "Set sail on luxurious cruises to explore multiple destinations, enjoy onboard entertainment, and experience the open sea.",
-    "image": "cruises.jpg"
+    "image": "https://i.postimg.cc/Y24NsQTg/boat.png"
   }
 ]
 
