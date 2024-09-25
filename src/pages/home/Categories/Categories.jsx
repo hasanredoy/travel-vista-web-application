55adf3f09ead99797df0a03cf17ab01b7e8e0b5e
unknown <redoyhasan4585@gmail.com>
2024-09-25 21:03:15 +0400

    displayed categories data in categories section

--- src/pages/home/Categories/Categories.jsx
+++ src/pages/home/Categories/Categories.jsx
@@ -5,11 +5,15 @@ import useLoadCategories from "@/hooks/categories/useLoadCategories";
 const Categories = () => {
   const categories = useLoadCategories()
   return (
-    <section>
-      <Heading t1={'Your Gateway to'} imp={' Diverse Adventures'} desc={'Discover a variety of travel experiences tailored to your interests.'}  ></Heading>
-      <div>
-        {categories?.map((category,index)=><div key={index}>
-              <img src={category?.icon} alt="" />
+    <section  className=" w-[94%] md:w-[90%] lg:w-[85%] mx-auto">
+      <Heading t1={'Your Gateway to'} imp={' Diverse Adventures'} desc={'Discover a variety of travel experiences belong interests.'}  ></Heading>
+      <div className=" flex items-center justify-center gap-20  my-10 flex-row">
+        {categories?.map((category,index)=><div  key={index}>
+              <div className=" w-32 h-32 rounded-full box-border  flex gap-2 flex-col items-center ">
+              <img className=" w-24 h-24 rounded-full " src={category?.image} alt="" />
+              <p className="">{category?.category}</p>
+              </div>
+
         </div>)}
       </div>
     </section>
