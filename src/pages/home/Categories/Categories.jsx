3efef67fa5ac0081e67929fea2d4558c7e181994
unknown <redoyhasan4585@gmail.com>
2024-09-30 12:40:37 +0400

    makes categories cards clickable

--- src/pages/home/Categories/Categories.jsx
+++ src/pages/home/Categories/Categories.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Heading from "@/components/reuseble/Heading";
 import useLoadCategories from "@/hooks/categories/useLoadCategories";
+import Link from "next/link";
 
 const Categories = () => {
   const categories = useLoadCategories()
@@ -8,13 +9,13 @@ const Categories = () => {
     <section  className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'Your Gateway to'} imp={' Diverse Adventures'} desc={'Discover a variety of travel experiences belong interests.'}  ></Heading>
       <div className=" flex items-center justify-center gap-20  my-10 flex-row">
-        {categories?.map((category,index)=><div  key={index}>
+        {categories?.map((category,index)=><Link href={'/'}  key={index}>
               <div title={category?.category} className=" w-32 h-32 l box-border  flex gap-2 flex-col items-center ">
               <img className=" w-24 h-24  " src={category?.image} alt="" />
               <p className="">{category?.category}</p>
               </div>
 
-        </div>)}
+        </Link>)}
       </div>
     </section>
   );
