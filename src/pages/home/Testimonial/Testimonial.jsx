7682a85b1118a4fccbe4e0dd3d29088bdd544c87
unknown <redoyhasan4585@gmail.com>
2024-10-06 13:52:55 +0400

    loaded testimonial data

--- src/pages/home/Testimonial/Testimonial.jsx
+++ src/pages/home/Testimonial/Testimonial.jsx
@@ -1,6 +1,10 @@
+'use client'
 import Heading from "@/components/reuseble/Heading";
+import useLoadTestimonial from "@/hooks/testimonial/useLoadTestimonial";
 
 const Testimonial = () => {
+  const testimonials = useLoadTestimonial()
+  console.log(testimonials);
   return (
     <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'Hear About '} imp={' Us'} ></Heading>
