ebde2dd6e63afca26c1a2666d31c3aab3796e08d
unknown <redoyhasan4585@gmail.com>
2024-10-07 12:31:43 +0400

    loaded testimonial data

--- src/pages/home/Testimonial/Testimonial.jsx
+++ src/pages/home/Testimonial/Testimonial.jsx
@@ -1,6 +1,19 @@
 'use client'
 import Heading from "@/components/reuseble/Heading";
 import useLoadTestimonial from "@/hooks/testimonial/useLoadTestimonial";
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import './testimonial.css';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 
 const Testimonial = () => {
   const testimonials = useLoadTestimonial()
@@ -8,6 +21,30 @@ const Testimonial = () => {
   return (
     <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'Hear About '} imp={' Us'} ></Heading>
+      <section>
+    
+      <Swiper
+        slidesPerView={'auto'}
+        centeredSlides={true}
+        spaceBetween={30}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+    
+      </section>
     </section>
   );
 };
