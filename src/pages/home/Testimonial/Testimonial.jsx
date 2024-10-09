82c27610cf766ca9fc0a117f09113a5ae7770015
unknown <redoyhasan4585@gmail.com>
2024-10-09 13:29:04 +0400

    fixing image in slider

--- src/pages/home/Testimonial/Testimonial.jsx
+++ src/pages/home/Testimonial/Testimonial.jsx
@@ -37,7 +37,7 @@ const Testimonial = () => {
         {testimonials?.map((testimonial, index)=><SwiperSlide key={index}>
         <div className="p-6 sm:p-12 dark:bg-gray-50 dark:text-gray-800">
 	<div className="flex flex-col space-y-4 md:space-y-0 md:space-x-6 md:flex-row">
-		<img src={testimonial?.image} alt="" className=" flex-shrink-0 w-24 h-24 border rounded-full md:justify-self-start dark:bg-gray-500 dark:border-gray-300" />
+		<img src={testimonial?.image} alt="" className="  w-24 h-24 border " />
 		<div className="flex flex-col">
 			<h4 className="text-lg font-semibold text-center md:text-left">{testimonial?.name}</h4>
 			<p className="dark:text-gray-600">Sed non nibh iaculis, posuere diam vitae, consectetur neque. Integer velit ligula, semper sed nisl in, cursus commodo elit. Pellentesque sit amet mi luctus ligula euismod lobortis ultricies et nibh.</p>
