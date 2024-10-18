f996036a1f1ee257115a2228830008bf2065627a
unknown <redoyhasan4585@gmail.com>
2024-10-18 16:31:25 +0400

    working on news letter section adding form

--- src/pages/home/Newsletter/NewsLetter.jsx
+++ src/pages/home/Newsletter/NewsLetter.jsx
@@ -3,6 +3,26 @@ const NewsLetter = () => {
   return (
     <section>
       <h1>Subscribe to Newsletter</h1>
+      <form className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
     </section>
   );
 };
