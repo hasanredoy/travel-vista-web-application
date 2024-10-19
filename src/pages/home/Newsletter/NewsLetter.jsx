a24608a24a42c33e9f1e41a0243925d402c95a0e
unknown <redoyhasan4585@gmail.com>
2024-10-19 14:37:47 +0400

    working on news letter section

--- src/pages/home/Newsletter/NewsLetter.jsx
+++ src/pages/home/Newsletter/NewsLetter.jsx
@@ -1,29 +1,23 @@
 
 const NewsLetter = () => {
   return (
-    <section>
-      <h1>Subscribe to Newsletter</h1>
+    <div
+  className="hero min-h-screen"
+  style={{
+    backgroundImage: "url(https://i.postimg.cc/2yvw21z6/email-network-communication-perforated-paper-sign.jpg)",
+  }}>
+  <div className="hero-overlay bg-opacity-60"></div>
+  <div className="hero-content text-neutral-content text-center">
+          <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto flex flex-col gap-10">
+    
+      <h1 className='text-4xl font-black'>Subscribe to Travel Vista! </h1>
       <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
+
       </form>
     </section>
+  </div>
+</div>
+
   );
 };
 
