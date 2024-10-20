1506ee716df41a9f763b781807275c4a1d161349
unknown <redoyhasan4585@gmail.com>
2024-10-20 21:02:43 +0400

    working on news letter section adding subscribe input

--- src/pages/home/Newsletter/NewsLetter.jsx
+++ src/pages/home/Newsletter/NewsLetter.jsx
@@ -2,17 +2,23 @@
 const NewsLetter = () => {
   return (
     <div
-  className="hero min-h-screen"
+  className="hero min-h-[calc(100dvh-200px)] "
   style={{
     backgroundImage: "url(https://i.postimg.cc/2yvw21z6/email-network-communication-perforated-paper-sign.jpg)",
   }}>
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-neutral-content text-center">
-          <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto flex flex-col gap-10">
+          <section >
     
       <h1 className='text-4xl font-black'>Subscribe to Travel Vista! </h1>
       <form className="card-body">
-
+      <div className="join">
+        <input
+          type="text"
+          placeholder="username@site.com"
+          className="input input-bordered join-item" />
+        <button className="btn btn-primary join-item">Subscribe</button>
+      </div>
       </form>
     </section>
   </div>
