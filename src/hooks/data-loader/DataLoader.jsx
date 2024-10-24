ab8b27349f493f305a65337b27400650b47d7e04
unknown <redoyhasan4585@gmail.com>
2024-10-24 14:26:17 +0400

    loaded our partners data

--- src/hooks/data-loader/DataLoader.jsx
+++ src/hooks/data-loader/DataLoader.jsx
@@ -2,12 +2,13 @@ import axios from "axios";
 
 const loadData = async(URL)=>{
       const res = await axios.get(URL)
-      console.log({URL});
-      console.log({response:res.data});
+      // console.log({response:res?.data?.data});
       return res?.data
 }
 const DataLoader = async (url) => {
       const Data = await loadData(`${process.env.NEXT_PUBLIC_BASE_URL}/api/${url}`)
+      // console.log('data from',Data);
+
   return Data
 };
 
--- src/pages/OurPartners/OurPartners.jsx
+++ src/pages/OurPartners/OurPartners.jsx
@@ -2,7 +2,6 @@ import DataLoader from "@/hooks/data-loader/DataLoader";
 
 const OurPartners =async () => {
   const ourPartners = await DataLoader('our-partners')
-  // console.log({ourPartners});
   return (
     <div>
       
