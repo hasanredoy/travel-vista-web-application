46ce4a2c580441d05ad3698a9a65a515a72ca839
unknown <redoyhasan4585@gmail.com>
2024-10-31 14:08:27 +0400

    fixed deployment issue in our partners section

--- src/pages/home/OurPartners/OurPartners.jsx
+++ src/pages/home/OurPartners/OurPartners.jsx
@@ -1,18 +1,34 @@
-/* eslint-disable @next/next/no-img-element */
-import Heading from "@/components/reuseble/Heading";
-import DataLoader from "@/hooks/data-loader/DataLoader";
-import Link from "next/link";
+// pages/our-partners.js
 
-const OurPartners = async() => {
-  const ourPartners = await DataLoader('our-partners')||[]
-  console.log(ourPartners);
+import Heading from "@/components/reuseble/Heading";
+import Link from "next/link";
+import DataLoader from "@/hooks/data-loader/DataLoader";
+
+export async function getStaticProps() {
+  const ourPartners = await DataLoader("our-partners") || [];
+  return {
+    props: {
+      ourPartners,
+    },
+    revalidate: 60, // Revalidate every 60 seconds for fresh data
+  };
+}
+
+const OurPartners = ({ ourPartners }) => {
   return (
-    <section className="  w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto ">
-      <Heading t1={'All our '} imp={' Partners'}></Heading>
+    <section className="w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
+      <Heading t1={'All our '} imp={' Partners'} />
       <div className="flex justify-center gap-10">
-      {ourPartners?.data?.map((partner,index)=><Link  href={partner?.website} key={index}>
-        <img src={partner.logo} alt="" title={partner?.name}  className='h-28 bg-white border p-5 border-gray-500 rounded-lg w-40' />
-      </Link>)}
+        {ourPartners?.data?.map((partner, index) => (
+          <Link href={partner?.website} key={index}>
+            <img
+              src={partner?.logo}
+              alt={partner?.name || "Partner logo"}
+              title={partner?.name}
+              className="h-28 bg-white border p-5 border-gray-500 rounded-lg w-40"
+            />
+          </Link>
+        ))}
       </div>
     </section>
   );
