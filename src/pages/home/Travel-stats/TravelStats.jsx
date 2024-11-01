823ec64e92ce63ecb37d83f3b2a83a5b0e684e54
unknown <redoyhasan4585@gmail.com>
2024-11-01 16:04:49 +0400

    added minimum and maximum width in travel stats section

--- src/pages/home/Travel-stats/TravelStats.jsx
+++ src/pages/home/Travel-stats/TravelStats.jsx
@@ -5,15 +5,17 @@ import DataLoader from "@/hooks/data-loader/DataLoader";
 const TravelStats = () => {
   const stats = DataLoader("travel-stats");
   return (
-    <section>
+    <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'Our'} imp={'Statics'}></Heading>
-      {stats.map((stat, index) => (
+  <div className=" flex gap-10">
+  {stats.map((stat, index) => (
         <div className=" flex flex-col justify-center gap-5" key={index}>
           <h1 className=" text-2xl font-black">{stat?.value}</h1>
           <h3 className=" text-xl font-semibold">{stat?.statistic}</h3>
           <p className=" text-lg">{stat?.description}</p>
         </div>
       ))}
+  </div>
     </section>
   );
 };
