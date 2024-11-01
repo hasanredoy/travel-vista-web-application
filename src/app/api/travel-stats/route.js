eccea355c023b0792aa46add8a054e7871ede414
unknown <redoyhasan4585@gmail.com>
2024-11-01 16:16:19 +0400

    added background and margin padding in travel stats

--- src/app/api/travel-stats/route.js
+++ src/app/api/travel-stats/route.js
@@ -16,15 +16,11 @@ const data = [
     "value": 120,
     "description": "Explore 120+ destinations worldwide, from bustling cities to serene escapes."
   },
-  {
-    "statistic": "Total Trips Booked",
-    "value": 540,
-    "description": "Over 54,000 unforgettable journeys planned and counting."
-  },
+ 
   {
     "statistic": "5-Star Reviews",
     "value": 350,
-    "description": "35,000+ positive reviews from satisfied travelers around the globe."
+    "description": "35,0+ positive reviews from satisfied travelers around the globe."
   },
   {
     "statistic": "Years of Service",
@@ -35,10 +31,6 @@ const data = [
     "statistic": "Countries Visited",
     "value": 50,
     "description": "Travel with us across 50 diverse countries and cultures."
-  },
-  {
-    "statistic": "Customer Satisfaction",
-    "value": 98,
-    "description": "98% customer satisfaction rating on travel experiences and service."
   }
+
 ]
--- src/pages/home/Travel-stats/TravelStats.jsx
+++ src/pages/home/Travel-stats/TravelStats.jsx
@@ -6,16 +6,25 @@ const TravelStats = () => {
   const stats = DataLoader("travel-stats");
   return (
     <section className=" w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
-      <Heading t1={'Our'} imp={'Statics'}></Heading>
-  <div className=" flex gap-10">
-  {stats.map((stat, index) => (
-        <div className=" flex flex-col justify-center gap-5" key={index}>
-          <h1 className=" text-2xl font-black">{stat?.value}</h1>
-          <h3 className=" text-xl font-semibold">{stat?.statistic}</h3>
-          <p className=" text-lg">{stat?.description}</p>
-        </div>
-      ))}
-  </div>
+      <Heading t1={"Our "} imp={" Statics"}></Heading>
+      <div className=" flex gap-10">
+        {stats.map((stat, index) => (
+          <div
+            key={index}
+            className={`flex flex-col items-center p-5 rounded-md shadow-md border border-sky-200 w-full  max-h-96 justify-center gap-5 
+             ${index == 0 && "bg-gradient-to-tr from-[#b3faee] via-[#fff] to-[#9bffff]"}
+             ${index == 1 && "bg-gradient-to-br from-[#f8b3fa] via-[#fff] to-[#f39bff]"}
+             ${index == 2 && "bg-gradient-to-tl from-[#b3faf6] via-[#fff] to-[#9bfff7]"}
+             ${index == 3 && "bg-gradient-to-bl from-[#f8b3fa] via-[#fff] to-[#f39bff]"}
+        
+        `}
+          >
+            <h1 className=" text-2xl font-black">{stat?.value}</h1>
+            <h3 className=" text-xl font-semibold">{stat?.statistic}</h3>
+            <p className=" text-lg">{stat?.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
