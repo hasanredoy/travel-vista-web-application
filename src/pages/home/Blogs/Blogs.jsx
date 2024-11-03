3e8a638229025fdc4172453527cae3f1a8071478
unknown <redoyhasan4585@gmail.com>
2024-11-03 18:41:22 +0400

    changed blog section route name

--- src/pages/home/Blogs/Blogs.jsx
+++ src/pages/home/Blogs/Blogs.jsx
@@ -1,9 +1,13 @@
+'use client'
+import DataLoader from "@/hooks/data-loader/DataLoader";
 
 const Blogs = () => {
+  const blogs = DataLoader('blog-section-data')
+  // console.log(blogs);
   return (
-    <div>
-      
-    </div>
+   <section>
+
+   </section>
   );
 };
 
