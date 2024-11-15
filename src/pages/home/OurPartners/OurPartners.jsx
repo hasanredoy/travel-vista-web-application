cfd0dddfdb72fd44331a1abdf8b63d7bf3dc188a
unknown <redoyhasan4585@gmail.com>
2024-11-15 14:50:07 +0400

    solved responsive issue in our patners section

--- src/pages/home/OurPartners/OurPartners.jsx
+++ src/pages/home/OurPartners/OurPartners.jsx
@@ -11,16 +11,16 @@ import Link from "next/link";
 const OurPartners = () => {
    const ourPartners = DataLoader('our-partners')
   return (
-    <section className="w-[94%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
+    <section className="w-[92%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'All our '} imp={' Partners'} />
-      <div className={` grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-center gap-10`}>
+      <div className={` md:w-[70%] mx-auto lg:w-[100%] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-center gap-6 place-items-center lg:gap-10`}>
         {ourPartners?.map((partner, index) => (
           <Link href={partner?.website} key={index}>
             <img
               src={partner?.logo}
               alt={partner?.name || "Partner logo"}
               title={partner?.name}
-              className="h-28 bg-white border p-5 border-gray-500 rounded-lg w-40"
+              className=" h-24 lg:h-28 bg-white border p-5 border-gray-500 rounded-lg w-32 lg:w-40"
             />
           </Link>
         ))}
