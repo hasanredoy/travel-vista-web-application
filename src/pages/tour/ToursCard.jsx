bc5379796d45a04324045902bf59b977e670a30a
unknown <redoyhasan4585@gmail.com>
2024-11-19 22:42:11 +0400

    making tours card

--- src/pages/tour/ToursCard.jsx
+++ src/pages/tour/ToursCard.jsx
@@ -1,9 +1,17 @@
 
 const ToursCard = ({tours}) => {
   return (
-    <main>
-      {tours.map((tour,index)=><div key={index}>
+    <main className=" grid grid-cols-2">
+      {tours.map((tour,index)=><div className="flex gap-10 w-full" key={index}>
+        {/* image section  */}
+        <section>
+          <img src={tour?.image} alt={tour?.title} />
+        </section>
+        {/* texts and others section  */}
+        <section>
+
         <h1>{tour?.title}</h1>
+        </section>
       </div>)}
     </main>
   );
