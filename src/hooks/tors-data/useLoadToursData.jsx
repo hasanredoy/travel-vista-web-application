23a71c89ef174a68beb28e22d93beca4c484879b
unknown <redoyhasan4585@gmail.com>
2024-11-19 21:09:01 +0400

    added hook to call tours data

---
+++ src/hooks/tors-data/useLoadToursData.jsx
@@ -0,0 +1,16 @@
+'use client'
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const useLoadToursData = () => {
+  const [data,setData] = useState([])
+  useEffect(()=>{
+    axios.get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/why-choose-us`)
+    .then(res=>{
+      setData(res?.data?.data)
+    })
+  },[])
+  return data||[]
+};
+
+export default useLoadToursData;
