b81df08a1245b572051f8916d6beded2ff6d104d
unknown <redoyhasan4585@gmail.com>
2024-11-20 21:52:46 +0400

    making tours card added title, description, price

--- src/pages/tour/ToursCard.jsx
+++ src/pages/tour/ToursCard.jsx
@@ -1,19 +1,34 @@
 /* eslint-disable @next/next/no-img-element */
 
-const ToursCard = ({tours}) => {
+const ToursCard = ({ tours }) => {
   return (
     <main className=" grid grid-cols-2 gap-20">
-      {tours.map((tour,index)=><div className=" rounded-e-md relative flex gap-10 w-full border shadow-lg bg-sky-800 bg-opacity-25 p-5 min-h-[300px] h-full" key={index}>
-        {/* image section  */}
-        <section className=" flex-1  ">
-          <img src={tour?.image} alt={tour?.title} className="absolute -left-5 w-[250px] h-[250px]" />
-        </section>
-        {/* texts and others section  */}
-        <section className=" flex-1">
-
-        <h1>{tour?.title}</h1>
-        </section>
-      </div>)}
+      {tours.map((tour, index) => (
+        <div
+          className=" rounded-e-md relative flex gap-10 w-full border shadow-lg bg-sky-800 bg-opacity-25 p-5 min-h-[300px] h-full"
+          key={index}
+        >
+          {/* image section  */}
+          <section className=" flex-1  ">
+            <img
+              src={tour?.image}
+              alt={tour?.title}
+              className="absolute -left-5 w-[250px] h-[250px]"
+            />
+          </section>
+          {/* texts and others section  */}
+          <section className=" flex-1">
+            <div>
+              <h1>{tour?.title}</h1>
+              <p>{tour?.description}</p>
+            </div>
+            <div>
+              <h3>Price: <span>{tour?.price}</span></h3>
+              <h3>Duration: <span>{tour?.package_duration}</span></h3>
+            </div>
+          </section>
+        </div>
+      ))}
     </main>
   );
 };
