5f23ea5ae6280f72fdec9acfcdd544ed5e2a60c9
unknown <redoyhasan4585@gmail.com>
2024-11-22 20:11:10 +0400

    added static tours card

--- src/pages/tour/ToursCard.jsx
+++ src/pages/tour/ToursCard.jsx
@@ -5,11 +5,11 @@ const ToursCard = ({ tours }) => {
     <main className=" grid grid-cols-2 gap-20">
       {tours.map((tour, index) => (
         <div
-          className=" rounded-e-md relative flex gap-10 w-full border shadow-lg bg-sky-800 bg-opacity-25 p-5 min-h-[300px] h-full"
+          className=" rounded-e-md relative flex gap-6 w-full border shadow-lg bg-base-300 bg-opacity-35 min-h-[300px] h-full"
           key={index}
         >
           {/* image section  */}
-          <section className=" flex-1  ">
+          <section className=" flex-1  p-5 pr-0 ">
             <img
               src={tour?.image}
               alt={tour?.title}
@@ -17,16 +17,18 @@ const ToursCard = ({ tours }) => {
             />
           </section>
           {/* texts and others section  */}
-          <section className=" flex-1">
+          <section className=" flex-1  p-5 pl-0  relative overflow-hidden ">
             <div>
-              <h1>{tour?.title}</h1>
-              <p>{tour?.description}</p>
+              <h1 className=" pb-3 text-xl font-semibold">{tour?.title}</h1>
+              <p className=" text-sm font-medium">{tour?.description}</p>
             </div>
-            <div>
-              <h3>Price: <span>{tour?.price}</span></h3>
-              <h3>Duration: <span>{tour?.package_duration}</span></h3>
+            <div className=" my-3">
+              <h3 className=" text-base font-medium pb-3 ">Price: <span className=" text-[#2bbfc4]">{tour?.price}</span></h3>
+              <h3 className=" text-base font-medium ">Duration: <span className=" text-[#2bbfc4]">{tour?.package_duration}</span></h3>
             </div>
-            <button></button>
+            <button className=" btn-primary mt-3">Details</button>
+            <div className=" bg-sky-200 w-20  h-20 border rotate-45 absolute -right-12 -bottom-12  "></div>
+            <div className=" bg-sky-200 w-20  h-20 border rotate-45 absolute -right-12 -top-12  "></div>
           </section>
         </div>
       ))}
