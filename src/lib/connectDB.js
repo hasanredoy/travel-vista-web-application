08ad019a0f8ed3300e3c9fd1523db29285107619
unknown <redoyhasan4585@gmail.com>
2024-11-23 22:44:34 +0400

    loaded tours data from db

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -1,7 +1,21 @@
-export const connectDB = ()=>{
-  let db;
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let db;
+export const connectDB = async ()=>{
+  if(db) return db;
   try {
-    
+    // get uri from env file 
+  const uri = process.env.NEXT_PUBLIC_MONGO_URI
+  console.log({uri});
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+  db=client.db('Travel-Vista')
+  return db;
   } catch (error) {
     console.log(error)
   }
