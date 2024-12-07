313d5ac1f8d98a7e906ec37f5482acb3eb007146
unknown <redoyhasan4585@gmail.com>
2024-12-07 22:38:23 +0400

    added loading spinner in tours page on suspense fall back

--- src/app/tours/page.jsx
+++ src/app/tours/page.jsx
@@ -1,9 +1,16 @@
 import Tours from "@/pages/tour/Tours";
 import { Suspense } from "react";
+import { FaSpinner } from "react-icons/fa";
 
 const page = () => {
   return (
-    <Suspense fallback={<span>Loading</span>}>
+    <Suspense
+      fallback={
+        <div className=" flex justify-center items-center min-h-screen">
+          <FaSpinner className=" text-xl animate-spin"></FaSpinner>
+        </div>
+      }
+    >
       <Tours></Tours>
     </Suspense>
   );
