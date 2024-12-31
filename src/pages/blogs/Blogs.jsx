b83829015b8489b8142278f00a65ca9eb9143cc9
unknown <redoyhasan4585@gmail.com>
2024-12-31 22:39:57 +0400

    changed user image and name

--- src/pages/blogs/Blogs.jsx
+++ src/pages/blogs/Blogs.jsx
@@ -15,10 +15,10 @@ const Blogs = () => {
      {/* user info section  */}
      <section className=" w-[30%]">
      <div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12 dark:bg-gray-50 dark:text-gray-800">
-	<img src="https://source.unsplash.com/150x150/?portrait?3" alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
+	<img src={user?.image} alt="" className="w-32 h-32 mx-auto rounded-full dark:bg-gray-500 aspect-square" />
 	<div className="space-y-4 text-center divide-y dark:divide-gray-300">
 		<div className="my-2 space-y-1">
-			<h2 className="text-xl font-semibold sm:text-2xl">Leroy Jenkins</h2>
+			<h2 className="text-xl font-semibold sm:text-2xl">{user?.name}</h2>
 			<p className="px-5 text-xs sm:text-base dark:text-gray-600">Full-stack developer</p>
 		</div>
 		<div className="flex justify-center pt-2 space-x-4 align-center">
