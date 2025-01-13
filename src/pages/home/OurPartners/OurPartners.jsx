09196b95ec395f94ac07c76b3f8e6aa7afea154d
unknown <redoyhasan4585@gmail.com>
2025-01-13 16:28:25 +0400

    fixed deployment issue

--- src/pages/home/OurPartners/OurPartners.jsx
+++ src/pages/home/OurPartners/OurPartners.jsx
@@ -1,19 +1,19 @@
-
+"use client"
 import Heading from "@/components/reuseble/Heading";
-import { loadData } from "@/utils/loadData";
+import useDataLoader from "@/hooks/data-loader/useDataLoader";
 import Link from "next/link";
 
 
-const OurPartners =async () => {
-   const ourPartners =await loadData('/our-partners')
+const OurPartners =() => {
+   const ourPartners = useDataLoader('/our-partners')||[]
   return (
     <section className="w-[92%] my-10 md:w-[90%] lg:w-[85%] mx-auto">
       <Heading t1={'All our '} imp={' Partners'} />
       <div className={` md:w-[70%] mx-auto lg:w-[100%] grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 justify-center gap-6 place-items-center lg:gap-10`}>
         {ourPartners?.map((partner, index) => (
-          <Link href={partner?.website} key={index}>
+          <Link href={partner?.website||'./hello.com'} key={index}>
             <img
-              src={partner?.logo}
+              src={partner?.logo||"https:partners.com"}
               alt={partner?.name || "Partner logo"}
               title={partner?.name}
               className=" h-24 lg:h-28 bg-white border p-5 border-gray-500 rounded-lg w-32 lg:w-40"
