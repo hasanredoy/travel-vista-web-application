cb7c0529b15e7801a5438f8e53568633b2c549d8
redoy-hasan <redoyhasan4585@gmail.com>
2025-02-02 15:42:51 +0400

    rendered all reviews as card

--- src/pages/reviews/Reviews.jsx
+++ src/pages/reviews/Reviews.jsx
@@ -1,22 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
-"use client"
-import useReviewsData from '@/hooks/reviews-data/useReviewsData';
-import { useSession } from 'next-auth/react'
+"use client";
+import useReviewsData from "@/hooks/reviews-data/useReviewsData";
+import moment from "moment";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
 
+const Reviews = () => {
+  // load reviews
+  const [reviews, loading] = useReviewsData();
 
-const Reviews=()=> {
+  //  get user from session
+  const { user } = useSession()?.data || {};
 
-  // load reviews 
-const [reviews,loading]= useReviewsData()
-
-  //  get user from session 
-  const {user} = useSession()?.data||{}
-  
   return (
-    <main className=" min-h-screen my-10  max-w-[90%]  lg:max-w-[85%] mx-auto">
-         {/* user info section */}
-         <section className=" w-[30%]">
+    <main className=" min-h-screen my-10  max-w-[90%]  lg:max-w-[85%] mx-auto flex">
+      {/* user info section */}
+      <section className=" w-[30%]">
         <div className=" bg-gradient-to-tr from-blue-200 via-sky-200 to-rose-200 p-0.5 max-w-xs rounded-xl ">
           <div className="flex flex-col justify-center max-w-xs p-6 shadow-md rounded-xl sm:px-12  bg-base-200 ">
             <img
@@ -69,35 +69,42 @@ const [reviews,loading]= useReviewsData()
       </section>
       {/* cards section  */}
       <section className=" w-[68%]">
-        <div>
-          {reviews?.map((review,index)=> <div key={index} className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-300 dark:bg-gray-50 dark:text-gray-800">
-            <div className="flex justify-between p-4">
-              <div className="flex space-x-4">
-                <div>
-                  <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+        <div className=" grid grid-cols-2 gap-6">
+          {reviews?.map((review, index) => (
+            <div
+              key={index}
+              className="container flex flex-col w-full max-w-lg mx-auto  rounded-md 
+          bg-base-200 "
+            >
+              <div className="p-4">
+                <div className="  text-sm md:text-base">
+                  <p>{review?.description}</p>
                 </div>
-                <div>
-                  <h4 className="font-bold">Leroy Jenkins</h4>
-                  <span className="text-xs dark:text-gray-600">2 days ago</span>
+                <div className={`border border-gray-300 ${review.description.length<100?'mb-6':"mb-2"} mt-4`}></div>
+                <div className="flex space-x-4">
+                  <div>
+                    <Image
+                      src={review?.image}
+                      alt="user image"
+                      width={48}
+                      height={48}
+                      className="object-cover w-12 h-12 rounded-full "
+                    />
+                  </div>
+                  <div>
+                    <h4 className="font-bold">{review?.user}</h4>
+                    <span className="text-xs text-green-600">
+                     {moment(review?.date).format("lll")}
+                    </span>
+                  </div>
                 </div>
               </div>
-              <div className="flex items-center space-x-2 dark:text-yellow-700">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
-                  <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
-                </svg>
-                <span className="text-xl font-bold">4.5</span>
-              </div>
             </div>
-            <div className="p-4 space-y-2 text-sm dark:text-gray-600">
-              <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
-              <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
-            </div>
-          </div>
-          )}
+          ))}
         </div>
-        </section>
+      </section>
     </main>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
